cb5161c95da9b053f58ad06cbfd620d50c1d2d40
Kem Belderol <kemuelclyd@gmail.com>
2019-08-04 08:18:23 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iTunesStoreApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iTunesStoreApp
+
+pod 'Alamofire'
+pod 'RealmSwift'
+
+end
---
+++ iTunesStoreApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesStoreApp
+//
+//  Created by Kem Belderol on 04/08/2019.
+//  Copyright © 2019 Krats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
